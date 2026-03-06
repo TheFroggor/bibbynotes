d4c91928feb9bba5a5812f726e6e6aae217f6215
bibby <bibby@bibby.rt>
2026-03-07 02:28:48 +0300

    fixed syntax errs

--- bibbynotes.c
+++ bibbynotes.c
@@ -6,13 +6,13 @@ int main(){
     const char *home = getenv("HOME");
     char notes_dir[256];
     snprintf(notes_dir, sizeof(notes_dir), "%s/BibbyNotes", home);
-    prep_folder_exists(notes_dir;
+    prep_folder_exists(notes_dir);
     int choice;
     do {
         printf("\n=== Bibby Notes ===\n");
-        printf("1. New Note\n"):
-        printf("2. Exit\n"):
-        printf("Choose: "):
+        printf("1. New Note\n");
+        printf("2. Exit\n");
+        printf("Choose: ");
         scanf("%d", &choice);
         while (getchar() != '\n');
         switch (choice) {
